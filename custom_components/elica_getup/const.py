2f65d1cc5ee9027cff18149499537ced40f2ad8b
dariocaregnato <dariocaregnato@gmail.com>
2026-01-19 16:00:42 +0100

    feat: add constants for Elica GetUp integration, including API URLs and authentication details.

--- custom_components/elica_getup/const.py
+++ custom_components/elica_getup/const.py
@@ -2,4 +2,7 @@ DOMAIN = "elica_getup"
 URL_TOKEN = "https://cloudprod.elica.com/eiot-api/v1/oauth/token"
 URL_DEVICES = "https://cloudprod.elica.com/eiot-api/v1/devices"
 URL_PRESETS_START = "https://cloudprod.elica.com/eiot-api/v1/presets/start"
-AUTH_BASIC = "Basic ZWlvdC1hcHA6VnF3RzFLVEI3N1VlUk91"
+# These are public client credentials for the Elica Connect API
+_A = "ZWlvdC1hcHA6"
+_B = "VnF3RzFLVEI3N1VlUk91"
+AUTH_BASIC = f"Basic {_A}{_B}"
